20b579bc1ac3230d77826ca788b81d7431c124f8
caciano <cacianogenz@hotmail.com>
2021-03-06 11:37:18 -0300

    init

---
+++ src/test/java/com/cs/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cs.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
